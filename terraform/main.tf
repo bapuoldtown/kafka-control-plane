7a87d53464f35a97ffd9a5af6d489f21c73c38cf
bapuoldtown <58926512+bapuoldtown@users.noreply.github.com>
2025-06-17 06:54:57 +0530

    Create main.tf
    main tf file commited

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "kafka_sg" {
+  name = "kafka-control-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2181
+    to_port     = 2181
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "kafka_node" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.kafka_sg.id]
+
+  tags = {
+    Name = "kafka-control-node"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo EC2 Created Successfully"]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+}
